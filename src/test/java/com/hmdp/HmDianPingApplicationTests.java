745b396d65d6f55eda04e4b728795b36cfb638d3
Zebra <zebra1573@163.com>
2025-08-08 16:21:26 +0800

    用Redis实现全局唯一id

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,10 +3,14 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -19,6 +23,35 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+
+        // CountDownLatch 就是等所有 300 个并发任务都跑完，再统计总耗时的同步工具。
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        // 主线程在这里阻塞等待，直到计数器减到 0（也就是 300 个任务都执行完）才会继续执行后面的代码。
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 
     @Test
     void testSaveShop() throws InterruptedException {
